30dbd64bb963d06056e3ebdd886dc4b102c0339d
Abel <abel.carril@upm.es>
2021-11-15 20:40:43 +0100

    v.2.70

---
+++ FFBatch/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FFBatch
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        ///
+
+        [STAThread]
+        private static void Main()
+        {
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGuid))
+            {
+                //if (!mutex.WaitOne(0, false))
+                //{
+                //}
+
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.EnableVisualStyles();
+                if (Environment.GetCommandLineArgs().Count() == 1)
+                {
+                    Form6 form_intro = new Form6();
+                    form_intro.Show();
+                    form_intro.Refresh();
+                }
+                new SingleInstanceApp().Run(Environment.GetCommandLineArgs());
+                //Application.Run(new Form1());
+            }
+        }
+
+        private static string appGuid = "01c90e3d-419d-4bf5-9fcc-fcebe6b16840";
+    }
+
+    internal class SingleInstanceApp : WindowsFormsApplicationBase
+    {
+        public SingleInstanceApp()
+            : base()
+        {
+            this.IsSingleInstance = true;
+        }
+
+        protected override void OnStartupNextInstance(
+            StartupNextInstanceEventArgs e)
+        {
+            base.OnStartupNextInstance(e);
+            string[] secondInstanceArgumens = e.CommandLine.Skip(1).ToArray();
+
+            String other_file = Path.GetTempPath() + "\\" + "FFBatch_test" + "\\" + "Other_instance.fftmp";
+            try
+            {
+                File.WriteAllLines(other_file, secondInstanceArgumens);
+            }
+            catch { }
+            // Handle command line arguments of second instance
+
+            //if (e.BringToForeground)
+            //{
+            this.MainForm.BringToFront();
+            //}
+        }
+
+        protected override void OnCreateMainForm()
+        {
+            base.OnCreateMainForm();
+            this.MainForm = new Form1();
+        }
+    }
+}
---
+++ FFBatch/Settings.cs
@@ -0,0 +1,30 @@
+﻿namespace FFBatch.Properties
+{
+    // Esta clase le permite controlar eventos específicos en la clase de configuración:
+    //  El evento SettingChanging se desencadena antes de cambiar un valor de configuración.
+    //  El evento PropertyChanged se desencadena después de cambiar el valor de configuración.
+    //  El evento SettingsLoaded se desencadena después de cargar los valores de configuración.
+    //  El evento SettingsSaving se desencadena antes de guardar los valores de configuración.
+    public sealed partial class Settings
+    {
+        public Settings()
+        {
+            // // Para agregar los controladores de eventos para guardar y cambiar la configuración, quite la marca de comentario de las líneas:
+            //
+            // this.SettingChanging += this.SettingChangingEventHandler;
+            //
+            // this.SettingsSaving += this.SettingsSavingEventHandler;
+            //
+        }
+
+        private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e)
+        {
+            // Agregar código para administrar aquí el evento SettingChangingEvent.
+        }
+
+        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Agregar código para administrar aquí el evento SettingsSaving.
+        }
+    }
+}
